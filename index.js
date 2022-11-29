6a233d7eec26bc79760825613419136be1dd2165
Kennedy-munene <kennedymunene309@gmail.com>
2022-11-29 12:52:36 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = "KENNEDY MUNENE"
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${'KENNEDY MUNENE'}!`
+
+const firstInitial = currentUser[0]
+const restOfName = currentUser .slice(1)
+
+const shortGreeting = `Welcome, ${firstInitial}!`
